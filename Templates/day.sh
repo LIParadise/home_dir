809aa87c63509e7e5ea7d29b7b563e30e59dd15c
LIParadise <a58524andy@gmail.com>
2019-04-10 00:32:51 +0800

    because gnome 3.30 nightlight have some serious bugs, this is the alternative.

---
+++ Templates/day.sh
@@ -0,0 +1,2 @@
+gsettings set org.gnome.settings-daemon.plugins.color night-light-schedule-from 0.0
+gsettings set org.gnome.settings-daemon.plugins.color night-light-schedule-to   23.983333333333331
---
+++ Templates/night.sh
@@ -0,0 +1,2 @@
+gsettings set org.gnome.settings-daemon.plugins.color night-light-schedule-from 23.0
+gsettings set org.gnome.settings-daemon.plugins.color night-light-schedule-to   3.0
