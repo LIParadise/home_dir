42dc64a9f0d2593c1390e71373ed4fb43379eee6
LIParadise <a58524andy@gmail.com>
2019-05-14 12:26:24 +0800

    added ac power check to 99-my_tuned_adm_modifier.sh

--- etc/pm/sleep.d/99-my_tuned_adm_modifier.sh
+++ etc/pm/sleep.d/99-my_tuned_adm_modifier.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
 case "$1" in
-suspend|suspend_hybrid|hibernate)
-  /usr/sbin/tuned-adm off
+  suspend|suspend_hybrid|hibernate)
+    /usr/sbin/tuned-adm off
     ;;
-resume|thaw)
+  resume|thaw)
+    ac_adapter=$(acpi -a | cut -d' ' -f3 | cut -d- -f1)
+    if [ "$ac_adapter" = "on" ]; then
+      /usr/sbin/tuned-adm off
+    else
+      /usr/sbin/tuned-adm profile laptop
+    fi
     ;;
 esac
