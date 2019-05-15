0ae73df79add8ab75a92ef05f80c693e5c2c9e4c
LIParadise <a58524andy@gmail.com>
2019-05-16 05:20:39 +0800

    added script s.t. it's easier to switch mode

--- Templates/tuned.sh
+++ Templates/tuned.sh
@@ -1,2 +1,12 @@
-#!/bin/bash
-tuned-adm profile laptop
+#!/usr/bin/env bash
+
+myac_adapter=$(acpi -a | cut -d' ' -f3 | cut -d- -f1)
+sleep 0.1 # to ensure it's off or on.
+echo "myac_adapter is $myac_adapter"
+if [ "$myac_adapter" = "on" ]; then
+  echo "switching off power-efficiency mode"
+  /usr/sbin/tuned-adm off
+else
+  echo "switching on power-efficiency mode"
+  /usr/sbin/tuned-adm profile laptop
+fi
