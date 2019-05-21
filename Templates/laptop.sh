661417554a2c0232c32f4dc0c0e3647770ea26d0
LIParadise <a58524andy@gmail.com>
2019-05-21 21:40:45 +0800

    changed exec order

--- Templates/laptop.sh
+++ Templates/laptop.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+# xrandr HDMI
+echo "xrandr --output HDMI-2 --set \"Broadcast RGB\" \"Full\""
+xrandr --output HDMI-2 --set "Broadcast RGB" "Full"
+
 # tuned-adm
 myac_adapter=$(acpi -a | cut -d' ' -f3 | cut -d- -f1)
 sleep 0.1 # to ensure it's off or on.
@@ -11,6 +15,3 @@ else
   echo "switching on power-efficiency mode"
   /usr/sbin/tuned-adm profile laptop
 fi
-
-# xrandr HDMI
-xrandr --output HDMI-2 --set "Broadcast RGB" "Full"
