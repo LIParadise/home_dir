2f9031a83d934aeee2ebf8140484aa2dc909f9dc
LIParadise <a58524andy@gmail.com>
2019-05-24 19:27:51 +0800

    param change: added scroll inertia to trackpoint evdev; delete some lines in laptop.sh

--- Templates/laptop.sh
+++ Templates/laptop.sh
@@ -38,3 +38,26 @@ case "${myTunedAdmProfile}" in
     echo -e "Power-Efficient Mode: ${myHiWhite}${myTunedAdmProfile}${myNoColour}"
     ;;
 esac
+
+# set xinput
+# I dunno where we xinput reads its configuration files
+# thus I put it here.
+#
+if [[ -n ${DISPLAY} ]]; then
+
+  command -v xinput >/dev/null 2>&1 || { echo >&2 "I require xinput but it's not installed.  Aborting."; exit 1; }
+
+  # check the following
+  # https://stackoverflow.com/questions/592620/how-to-check-if-a-program-exists-from-a-bash-script
+  # https://wiki.archlinux.org/index.php/TrackPoint#Middle_button_scroll
+  # for why this works.
+  # basically this is to prevent when "xinput" is absent
+  # the pros of this is it handles exit codes better then the commonly used util "which"
+  # and that this is posix-compatible.
+
+  xinput set-prop "SynPS/2 Synaptics TouchPad" "Device Accel Constant Deceleration" "2.5"
+
+fi
+#
+# set xinput end
+#
