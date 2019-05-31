9e00bb3d3ab0d49d22aab2798218966528e7dde9
LIParadise <a58524andy@gmail.com>
2019-06-01 02:01:59 +0800

    make some statement more specific in the script

--- Templates/monitor.sh
+++ Templates/monitor.sh
@@ -1,2 +1,9 @@
 #!/usr/bin/env bash
-watch -n 5 "sensors && lscpu | grep -iEe \"cpu mhz\""
+watch -n 5 "\
+  sensors -j |\
+    grep -iEe \"package id 0\" -A 1 |\
+    sed -E -e \"s/[[:blank:],{}]|//g\" &&\
+  echo \"\" &&\
+  lscpu | grep -iEe \"cpu mhz\" &&\
+  echo \"\" &&\
+  acpi -V | grep -iEe \"battery [0-1]\""
