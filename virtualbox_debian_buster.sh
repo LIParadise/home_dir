ca697f9eeea56a8fb06fa7afe1de877c3c2b59a7
LIParadise <a58524andy@gmail.com>
2019-07-14 19:20:16 +0800

    added how to add user to "vboxusers" group

--- virtualbox_debian_buster.sh
+++ virtualbox_debian_buster.sh
@@ -3,3 +3,4 @@ wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | sudo apt-
 wget -q https://www.virtualbox.org/download/oracle_vbox.asc -O- | sudo apt-key add -
 add-apt-repository "deb http://download.virtualbox.org/virtualbox/debian bionic contrib"
 # // debian buster and ubuntu boinc shares the repository, thus the name "bionic"
+# usermod -a -G vboxusers liparadise
