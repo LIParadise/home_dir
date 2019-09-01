9bd8b8774c8f921c569bc77134386a384bd831a7
LIParadise <a58524andy@gmail.com>
2019-09-02 03:12:06 +0800

    name change

--- Templates/72hz.sh
+++ Templates/72hz.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 xrandr --newmode "72hz" 210.25  1920 2056 2256 2592  1080 1083 1088 1128 -hsync +vsync
-xrandr --addmode eDP-1 72hz
-xrandr --output eDP-1 --mode "72hz"
+xrandr --addmode eDP1 72hz
+xrandr --output eDP1 --mode "72hz"
