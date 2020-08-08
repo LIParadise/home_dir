62bf08bf573ce764dbab23c301162b5a64f01fc9
LIParadise <a58524andy>
2020-08-09 01:50:04 +0800

    basic system monitoring script using `perl`, `sed`, `grep`, `acpi`, and `sensors`.
    tested on Archlinux on T480
    `uname -a` output:
    ``` bash
    Linux T480-arch 5.7.12-arch1-1 #1 SMP PREEMPT Fri, 31 Jul 2020 17:38:22 +0000 x86_64 GNU/Linux
    ```

--- Templates/monitor.sh
+++ Templates/monitor.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 watch -n 1 "\
-  sensors -j 2>&1 |\
-    grep -i -E -e \"package id 0\" -A 1 |\
-    sed -r \"s/[[:blank:],{}]|//g\" &&\
-  echo \"\" &&\
-  lscpu | grep -iEe \"cpu mhz\" &&\
-  echo \"\" &&\
-  acpi -V | grep -iEe \"battery [0-1]\""
+   sensors -u 2>&1 |\
+   grep -A 1 \"Package id 0\" | grep temp1_input | sed -r 's/[[:space:]]+temp1_input/CPU Package/' &&\
+   echo \"\" &&\
+   \
+   cat /proc/cpuinfo | grep \"processor\|cpu MHz\" | perl -0777 -pe 's/processor[[:space:]]+:[[:space:]]+([[:digit:]]+).cpu MHz[[:space:]]+:[[:space:]]+([0-9.]+)/core\$1: \$2 MHz/gms' &&\
+   echo \"\" &&\
+   \
+   acpi -V | grep Battery"
