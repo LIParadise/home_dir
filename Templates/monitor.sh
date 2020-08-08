74fb854a6aca0ebe564136d696ba9f8932a10387
LIParadise <a58524andy>
2020-08-09 04:27:43 +0800

    performance tune (hopefullly) at the cost of readability

--- Templates/monitor.sh
+++ Templates/monitor.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
 watch -n 1 "\
-   sensors -u 2>&1 |\
-   grep -A 1 \"Package id 0\" | grep temp1_input | sed -r 's/[[:space:]]+temp1_input/CPU Package/' &&\
-   echo \"\" &&\
+   sensors -u 2>&1 | grep -A 1 \"Package id 0\" | grep temp1_input | rev | cut -d ' ' -f1 | rev | awk '{print \"CPU Package: \" \$0}' &&\
+   printf \"\n\" &&\
    \
-   cat /proc/cpuinfo | grep \"processor\|cpu MHz\" | perl -0777 -pe 's/processor[[:space:]]+:[[:space:]]+([[:digit:]]+).cpu MHz[[:space:]]+:[[:space:]]+([0-9.]+)/core\$1: \$2 MHz/gms' &&\
-   echo \"\" &&\
+   grep \"processor\|cpu MHz\" /proc/cpuinfo | rev | cut -d' ' -f 1 | rev | paste -d ' '  - - &&\
+   printf \"\n\" &&\
    \
    acpi -V | grep Battery"
