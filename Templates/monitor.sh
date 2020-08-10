5bb60587ef4b4d068985e2a57e189729b11f120d
LIParadise <a58524andy>
2020-08-10 20:55:41 +0800

    `acpi` is powerful

--- Templates/monitor.sh
+++ Templates/monitor.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 watch -n 1 "\
-   sensors -u 2>&1 | grep -A 1 \"Package id 0\" | grep temp1_input | rev | cut -d ' ' -f1 | rev | awk '{print \"CPU Package: \" \$0}' &&\
+   acpi -t | sed -r 's/ok, //' &&\
    printf \"\n\" &&\
    \
    grep \"processor\|cpu MHz\" /proc/cpuinfo | rev | cut -d' ' -f 1 | rev | paste -d ' '  - - &&\
