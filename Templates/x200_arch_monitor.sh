18959bb6942bec3fa78fe425936aca7d1c34a9cc
LIParadise <a58524andy@gmail.com>
2020-08-11 00:46:59 +0800

    specify sensor name instead of all

--- Templates/x200_arch_monitor.sh
+++ Templates/x200_arch_monitor.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 watch -n 1 "\
-   sensors -A 2>&1 | grep Core | awk '{print \$1\$2\" \"\$3}' &&\
+   sensors acpitz-acpi-0 2>&1 | grep temp | awk '{print \$1\" \"\$2}' &&\
    printf \"\n\" &&\
    \
    grep \"processor\|cpu MHz\" /proc/cpuinfo | rev | cut -d' ' -f 1 | rev | paste -d ' '  - - &&\
