50b1cc4fa6dbf548214e5ddf890bba24058c9d59
LIParadise <a58524andy@gmail.com>
2021-05-14 11:36:30 +0800

    simple script for updating personal utilities
    `zsh` plugins
    `neovim` plugins
    `rustup`

---
+++ Templates/update_things.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env zsh
+
+simple_git_update(){
+    git remote -v update
+    git pull
+}
+
+rustup self update && rustup update &
+
+omz update
+cd ${HOME}/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+simple_git_update &
+cd ${HOME}/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+simple_git_update &
+cd
+
+nvim +PlugUpgrade +qall
+nvim +PlugUpdate  +qall
+nvim +CocUpdate   +qall
+
+wait
