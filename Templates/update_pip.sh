76ba2287275d8788b080d2080628ffc0f28b37e8
LIParadise <a58524andy@gmail.com>
2021-11-13 18:36:29 +0800

    `pip` update is dangerous in general (IMO)

--- Templates/update_pip.sh
+++ Templates/update_pip.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env sh
+if [ "$VIRTUAL_ENV" = "" ]; then
+    echo "You need to be in virtualenv!!!"
+    exit 1
+else
+    echo "In virtualenv $VIRTUAL_ENV"
+fi
+
 python -m pip install --upgrade pip
 pip list --outdated --format=freeze | grep -v '^\-e' | cut -d = -f 1  | xargs -n1 pip install -U
