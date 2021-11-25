170e7a739833aadca87242bb969a49e8362c0c47
LIParadise <a58524andy@gmail.com>
2021-11-26 02:28:28 +0800

    Don't need to set again variables when starting `sway`

--- .zsh/.zshenv
+++ .zsh/.zshenv
@@ -1,5 +1,7 @@
 . "$HOME/.cargo/env"
+export XMODIFIERS="@im=fcitx"
 export GTK_IM_MODULE=fcitx
 export QT_IM_MODULE=fcitx
-export XMODIFIERS=@im=fcitx
+export XIM=fcitx
+export XIM_ARGS="-rd"
 export MOZ_ENABLE_WAYLAND=1
--- Templates/sway.sh
+++ Templates/sway.sh
@@ -1,13 +1,4 @@
 #!/usr/bin/env dash
 if [ -z "$DISPLAY" ] && [ "$(tty)" = "/dev/tty3" ] ; then
-   export XMODIFIERS="@im=fcitx"
-   export GTK_IM_MODULE=fcitx
-   export QT_IM_MODULE=fcitx
-   export XIM=fcitx
-   export XIM_ARGS="-rd"
-   # ${XIM_PROGRAM} ${XIM_ARGS}
-   # export DEPENDS="ibus"
-   # export XIM_PROGRAM=/usr/bin/ibus-daemon
-   # export XIM_PROGRAM_SETS_ITSELF_AS_DAEMON=yes
    exec env sway
 fi
