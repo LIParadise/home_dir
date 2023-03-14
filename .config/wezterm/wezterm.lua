e68fdf89500289df010fbde4a2cff86be7799c92
LIParadise <a58524andy@gmail.com>
2023-03-15 01:07:28 +0800

    [wezterm] remove unnecessary key bindings
    Should've RTFM

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -3,9 +3,6 @@ return {
     font = wezterm.font 'JetBrains Mono',
     font_size = 14.0,
     window_decorations = "RESIZE",
-    keys = {
-        { key = 'F11', mods = '', action = wezterm.action.ToggleFullScreen, },
-    }
     color_scheme = "Eighties (base16)",
-    scrollback_lines = 1048576,
+    scrollback_lines = 1048576
 }
