59061e36519cff3046391c115db118a7e8ccc76b
LIParadise <a58524andy@gmail.com>
2023-07-03 00:17:54 +0800

    [WSL] [WSL2] Mount drives to WSL2 upon start
    Need to import the `.xml` to Windows task scheduler.
    Utilizing `wsl mount --bare` to access filesystems that doesn't have
    official driver under Windows.

---
+++ wsl/liparadise_5200pro.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# https://github.com/microsoft/WSL/issues/6073#issuecomment-1266405095
+/bin/bash /liparadise_5200pro_worker.sh > /liparadise_5200pro.log 2>&1
---
+++ wsl/liparadise_5200pro_worker.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# This will be executed whenever WSL starts.
+
+# Execute the task we created before.
+/mnt/c/Windows/system32/schtasks.exe /run /tn "wsl_mount_5200pro"
+# Since mounting will take some time, if you need to do something with the mounted drives later,
+# you will need to write some logic to wait for it to be ready.
