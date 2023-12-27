536ef0bd7a1f2e70e96f3c7ad6761d91ce052ef5
LIParadise <a58524andy@gmail.com>
2023-12-27 23:23:01 +0800

    compatibility

--- Templates/gen_passwd.sh
+++ Templates/gen_passwd.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
 
 if [ $# -eq 1 ] && [ "$1" -ge 1 ]; then
-    tr -dc 'A-Za-z0-9!"#$%&'\''()*+,-./:;<=>?@[\]^_`{|}~' </dev/urandom | head -c "$1"; echo
+    LC_ALL=C tr -dc 'A-Za-z0-9!"#$%&'\''()*+,-./:;<=>?@[\]^_`{|}~' </dev/urandom | head -c "$1"; echo
 fi
